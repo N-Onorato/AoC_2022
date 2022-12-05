e45e3dd25049364ef1848532fdd208262e8c3435
Nick Onorato <N_onorato@outlook.com>
2022-12-05 12:55:32 -0700

    day 5 setup

---
+++ day_05/solution.py
@@ -0,0 +1,17 @@
+from collections import deque
+import re
+r"/ss"
+
+initial = [
+    deque([]),
+    deque("BGSC"),
+    deque("TMWHJNVG"),
+    deque("MQS"),
+    deque("BSLTWNM"),
+    deque("JZFTVGWP"),
+    deque("CTBGQHS"),
+    deque("TJPBW"),
+    deque("GDCZFTQM"),
+    deque("NSHBPF")
+]
+
